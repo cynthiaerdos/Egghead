0d83152d7df596629417a5c6a559dea09f487f2f
rerdos <ricsi96@gmail.com>
2014-12-07 16:43:00 +0100

    New videos up tp 14

--- projects/4-sharing-data-between-controllers/main.js
+++ projects/4-sharing-data-between-controllers/main.js
@@ -1,11 +1,11 @@
 var myApp = angular.module('myApp', []);
-myApp.factory('Data', function(){
+myApp.factory('richardData', function(){
     return {message:"I'm global!"};
 });
-function FirstCtrl($scope, Data){
-    $scope.data = Data;
+function FirstCtrl($scope, richardData){
+    $scope.data = richardData;
 }
 
-function SecondCtrl($scope, Data){
-    $scope.data = Data;
+function SecondCtrl($scope, richardData){
+    $scope.data = richardData;
 }
