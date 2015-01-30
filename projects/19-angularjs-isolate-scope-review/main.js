2a4aeacced352f3c95b56c64e151035ecf38a3a7
rerdos <ricsi96@gmail.com>
2015-01-30 20:14:58 +0100

    Isolated scope review

---
+++ projects/19-angularjs-isolate-scope-review/main.js
@@ -0,0 +1,25 @@
+var app = angular.module("phoneApp", []);
+
+app.controller("AppCtrl", function($scope) {
+    $scope.callHome = function(number, network, message) {
+        alert("Number: " + number + " network: " + network + " message: " + message + ".")
+    }
+})
+
+app.directive("phone", function() {
+    return {
+        restrict: "E",
+        scope: {
+            number: "@",
+            network: "=", // It won't work if you change the 'network' to anything else and use that below in the template.
+            makeCall: "&"
+        },
+        template: '<div class="panel">Number: {{number}} | Network: {{network}} <select ng-model="network" ng-options="net for net in networks"></select>' +
+            '<input type="text" ng-model="value">' +
+            '<div class="button" ng-click="makeCall({number:number,network:network,message:value})">Call home!</div></div>',
+        link: function(scope){
+            scope.networks = ["Giffgaff", "O2", "T-mobile", "Vodafone"];
+            scope.network = scope.networks[0];
+        }
+    }
+})
